706f1fa4a6c3a296d4275a0307ef3f19d7e139bb
Joseph <joseph.kodos@gmail.com>
2019-03-22 06:34:35 -0400

    latest notes

--- script_connector.py
+++ script_connector.py
@@ -9,18 +9,20 @@ ebay: mini project list
 """
 
 """
-
+visionapp - rdp into multiple sessions at once
 local db <---> physical medium <--> local db
 
-built PC sales - ebay
-built PC sales - local
-crypto mining
-pc building
+pc_building
+    built PC sales - ebay
+    built PC sales - local
+    crypto mining
     hardware renting
+    email_server
     runescape bot farm
         scripts
             rs_bot_api miniproject
             rs_bot_farm scripts (rs_bot_api profile + gsheet_api profile + sequencer profile)
+                account_manager (list of accounts and info about them)
                 schedule_manager (list of schedules and info about them)
                 cycle_manager (list of cycles and info about them)
                 task_manager (list of tasks and info about them)
@@ -32,26 +34,41 @@ pc building
                 environment_deploy (prep OS first time use: set network+install RuneLite+RSScripts + set firewall, etc)
                 environment_run scripts (to launch the VM+OS+RuneLite+RSScripts+rs_bot_farm scripts)
                 rs_bot_farm_manager (summarized data of all PCs, live PC stats, VMs, accounts, active schedules, etc)
-                
-                
-ebay bulk sales
+ebay_bulk_sales
 scripts
     ebay_api miniproject
     gsheet_api miniproject
     api_sequencer miniproject
     ebay_inventory scripts (gsheet_api profile + ebay object profile + sequencer profile)
-    
     pc inventory ebay scripts (PC inventory <--> ebay objects)
     pc built-to-order ebay scripts (PC built-to-order inventory <--> ebay objects)
     pc inventory to built-to-order migrator scripts (PC built-to-order inventory <--> built to order PC inventory)
-    
     bittrex_api trading scripts (convert from google scripts to python)
-    
     flask_ui project
         ebay item inventory
         pc item and builds inventory
         rs_bot_farm_manager (dashboard of all PCs, live PC stats, VMs, accounts, active schedules, etc)
-    
     built PC sales - local - description generator
+research
+    economy
+    world events
+    agendas
+    prophecies
+    self-sustainability
+    winter wilderness survival
+flee_prep
+    safehouse stockpile (acquiring and storing goods)
+    item inventory (determining what to get)
+    logistics (where to go)
+    meet ups
+rented_location
+    lan center
+    pc_building
+    ebay_bulk_sales
+business proposal
+    for-profit
+        investor_list
+    non-profit
+        kickstarter
 
 """

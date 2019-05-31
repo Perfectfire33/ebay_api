4720f5a16fa14fac8b0e3504cc0ebfb3510b6ac0
Perfectfire33 <perfectfireiii@gmail.com>
2019-05-31 05:34:43 -0400

    aaa
    aaa

--- ebay_object_receiver.py
+++ ebay_object_receiver.py
@@ -34,17 +34,87 @@ headers.append('packed_item_height')
 headers.append('packed_item_length')
 headers.append('packed_item_depth')
 
-# ebay incoming object row
-inventory_row = {}
+#group of headers
+header_group = []
+header_group_ct = []
+header_group_data = []
 
-# need to define column groups
+# switch var -- only run once for each group
+append_new_group = 0
+
+# for each group of columns in the incoming data set:
+for selected_columns in dataSet:
+    print("selected_columns")
+    print(selected_columns)
+
+    # only run once for each group -- turn on
+    append_new_group = 1
+    # header_group_data.append()
+
+    for column in selected_columns:
+        print("column")
+        print(column)
+        header_group_data.append(column)
+        print("len(column)")
+        print(len(column))
+        if append_new_group == 1:
+            header_group_ct.append(len(column))
+
+        # turn off after once run
+        append_new_group = 0
+
+
+print("header_group_ct")
+print(header_group_ct)
+
+for area_group in header_group_ct:
+    # reset_i_a
+    i_a = 1
+    header_group.append(area_group)
+    while i_a <= area_group:
+        print("i_a: " + str(i_a) + "   " + "area_group: " + str(area_group))
+        header_group[area_group].append(headers[i_a])
+        i_a = i_a + 1
+    # reset i_a
+    i_a = 1
+
+
+
+
+print("header_group")
+print(header_group)
+print("header_group[area_group]")
+print(header_group[1])
+
+
+
+header_group_data = dataSet[0][0]
+print("header_group_data")
+print(header_group_data)
+
+
+
+# get header groups
+for count in len(dataSet):
+    header_group_ct = len(dataSet[0][0])
+
+
+
+# need to define header groups
+for header_object in headers:
+    header_group.append(header_object)
+
+
 
 
 # set to first group of columns
-column_group = 0
+column_group_ct = 0
+
+# ebay incoming object row
+inventory_row = {}
 
 # get number of rows to get this header
-rows = len(dataSet[column_group])
+rows = len(dataSet[column_group_ct])
 
 # select first column
 column = 0
@@ -54,7 +124,7 @@ column = 0
 
 for row in rows:
         for header in headers:
-            inventory_row[header] = dataSet[column_group][row][column]
+            inventory_row[header] = dataSet[column_group_ct][row][column]
 
 
 """

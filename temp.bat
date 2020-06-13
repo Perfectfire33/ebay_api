5166b865ff89346df4e468542e0ace9630a9a34c
Perfectfire33 <perfectfireiii@gmail.com>
2020-06-13 12:17:30 -0400

    1a
    a1

---
+++ temp.bat
@@ -0,0 +1,101 @@
+every SAT course
+I want to understand the IT world thoroughly so I can set up my own business and services if push comes to shove.
+class notes would be helpful over searching online haha
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+Hmm, I see... alrighty then! I'm going to break this down since there is a lot of data here. 
+====================================================================================================================================
+class notes would be helpful over searching online haha
+-----
+Alright, I got all my class files syncing to my MTU Google Drive. It amounts to about 1 GB compressed. 
+What is your email address that I should share this with?
+
+Also, unfortunately, there are a lot of notes that had been written so I cannot share those easily. 
+But I think the syllabi are a great place to start... follow those to get ideas of what to learn.
+
+
+: The following is best viewed in Notepad++ with the Language set to Batch
+====================================================================================================================================
+every SAT course
+-----
+Here is every course in 2011-12 CNSA Flowchart and what you learn in it, essentially:
+(the flowchart itself will be shared in the Courses folder)
+------- ------------------------------  ----------------------------------------------------------------------
+SAT1200 Intro to Programming            C Language
+SAT1610 Computer & OS Architecture      Assembly Language, Basic PC Hardware
+SAT1700 Cyber Ethics                    Ethics and IT
+SAT2343 Network Administration I        Make cables, Use Packet Tracer to Configure and Emulate Cisco IOS
+SAT3343 Network Administration II       Use Real Hardware to Configure Cisco ISO
+SAT2711 Unix & Linux Administration I   Using Linux O/S (Fedora/RHEL) & Server Config (DNS, DHCP, etc)
+SAT3711 Unix & Linux Administration II  Using Linux O/S (Fedora/RHEL) & Server Config (NIS, NFS, etc)
+SAT2511 MS System Administration I      Using Windows O/S (Server2008R2) & Server Config (DNS, DHCP, etc)
+SAT3511 MS System Administration II     Using Windows O/S (Server2008R2) & Server Config (AD, SMB/CIFS, etc)
+SAT3210 Database Administration         Using and Configuring Databases (Oracle, Custom)
+SAT3812 Network Engineering Security I  Algorithms, Methods of Security (IDS/IPS), Hacking, etc
+SAT3820 Wireless "Mobile & FCC"         Wireless Communication Protocols, Wireless Site Survey, etc
+SAT3200 Storage Area Networking         Using and Configuring a SAN, Basic Concepts
+SAT3900 NewTechnologiesSeminars         Technology
+SAT3901 BehavioralAnalysis              People
+SAT4541 LDAP                            389/Fedora Directory Server, Custom
+SAT4600 WebApp/SrvAdmin                 LAMP/WAMP/Custom Server Config & Deploy, Custom
+SAT4480 SeniorProjectI                  ENTERPRISE:ITOxygen/Virtualization&Cloud/CloudStack on XenServer
+SAT4880 SeniorProjectII                 ENTERPRISE:ITOxygen/Virtualization&Cloud/OpenStack on XenServer
+
+====================================================================================================================================
+I want to understand the IT world thoroughly...
+--------------------------------------------------------------------------
+Hmm, this is a big idea but it is possible!
+Here is my interpretation of what might need to be done to understand the IT world thoroughly:
+
+|=>{"to understand the IT world thoroughly"};
+^
+|=>Summarize Meaning of IT world
+^
+|>   Reflection Upon Gained Experience -->|
+|                                         V
+|                               Gain Experience through Learning
+|                               Gain Experience through Planning
+|                               Gain Experience through Doing
+|
+|>   Understand Relationships Between IT World Components -->|
+|>   Understand Role Each IT World Component Assumes      -->|
+               |>Understand the Purpose of Each Role         |>Identify IT World Components
+                                                                |>Find Sources
+                                                                        >Source0:"every SAT course";
+                                                                        
+--------------------------------------------------------------------------
+...so I can set up my own business and services if push comes to shove.
+--------------------------------------------------------------------------
+Yes, setting up IT infrastructure and understanding how it works is critical to setting up a business.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
